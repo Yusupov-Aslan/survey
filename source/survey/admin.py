c273d30141843b1f252c8a0d8910e3241fab84d1
ваше Yusupov-Aslan <yusupovaslan4@gmail.com>
2022-02-05 12:20:21 +0600

    Создал модель Опроса (Poll) и модель Варианта ответа (Choice) так же зарегал модели в админ-панели и добавил тестовые данные, создал фикстуру.

--- source/survey/admin.py
+++ source/survey/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from survey.models import Poll, Choice
+
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['id', 'question', 'created_at']
+    list_filter = ['question']
+    fields = ['question']
+    readonly_fields = ['created_at']
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
